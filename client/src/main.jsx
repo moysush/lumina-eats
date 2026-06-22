37a37724563133b49648bcba865805360a15775f
Mahmud Hossain Sushmoy <sushmoy@duck.com>
2026-06-23 01:54:05 +0600

    setup mantine css library with luminaYellow theme

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import "./index.css";
+import App from "./App.jsx";
+import "@mantine/core/styles.css";
 
-createRoot(document.getElementById('root')).render(
+import { MantineProvider, createTheme } from "@mantine/core";
+
+const luminaYellow = [
+  "#fdfce4",
+  "#f8f6d3",
+  "#f0ecaa",
+  "#e7e17c",
+  "#e0d856",
+  "#dbd33e",
+  "#d9d02f",
+  "#c0b820",
+  "#aaa317",
+  "#928d03",
+];
+
+const theme = createTheme({
+  colors: {
+    luminaYellow,
+  },
+  primaryColor: "luminaYellow",
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <MantineProvider defaultColorScheme="auto" theme={theme}>
+      <App />
+    </MantineProvider>
   </StrictMode>,
-)
+);
